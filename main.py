c643022bd34be27a2e541054fcd752dacdb69756
Nuffe <carl.j.paulsson@hotmail.com>
2026-08-06 22:00:01 +0200

    counting total words per file

--- main.py
+++ main.py
@@ -7,39 +7,41 @@ folder = "test-files"
 
 print("Word to search:")
 search_word_user = input().lower()
-search_word_hard = "text".lower()
-mainDict = {}
+wordDict = {}
+fileDict = {}
 
 
-def add_to_dict(word_line, file):
-    word_list = word_line.replace("_", " ").replace("-", " ") \
-    .translate(str.maketrans('', '', string.punctuation)).split() 
+def add_to_dict(word_list, file):
+ 
     for word in word_list:
         word = word.lower()
-        if word not in mainDict:
-            mainDict[word] = {}
-        if file.name in mainDict[word]:
+        if word not in wordDict:
+            wordDict[word] = {}
+        if file.name in wordDict[word]:
             # Counting amount, probebly add aditional variables later for ranking
-            mainDict[word][file.name] += 1 
+            wordDict[word][file.name] += 1 
         else:
-            mainDict[word][file.name] = 1
+            wordDict[word][file.name] = 1
 
 
 with os.scandir(folder) as files:
     for file in files:
         if file.is_file() and file.name.endswith(".txt"):
-            with open(file.path, "r") as file:
-                for line in file:   
-                    add_to_dict(line, file)
-
+            with open(file.path, "r") as openFile:  
                 #Same to file name as for each line
                 file_name = pathlib.Path(file.name).stem
                 add_to_dict(file_name, file)
+                
+                for line in openFile:  
+                    word_list = line.replace("_", " ").replace("-", " ") \
+                         .translate(str.maketrans('', '', string.punctuation)).split()
+                    if file_name not in fileDict:
+                        fileDict[file_name] = 0
+                    fileDict[file_name] += len(word_list)
+                    add_to_dict(word_list, openFile)
 
 
 print(f"Searched word: {search_word_user}")
 print("Files:")
-for file in mainDict.get(search_word_user, {}):
-    print(f"{pathlib.Path(file).stem}, {search_word_user} appears {mainDict[search_word_user][file]} times")
-
-
+for file in wordDict.get(search_word_user, {}):
+    print(f"{pathlib.Path(file).stem}, {search_word_user} appears {wordDict[search_word_user][file]}/{fileDict[pathlib.Path(file).stem]} words")
