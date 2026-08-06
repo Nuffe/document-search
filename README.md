ba81778beb0bfffcd2649e8340b2ce2c84144498
Nuffe <carl.j.paulsson@hotmail.com>
2026-08-06 10:12:26 +0200

    Cleaning up word split and centralising it in the function

--- README.md
+++ README.md
@@ -3,9 +3,9 @@ Plan/thought:
 Input: a directory of files together with a word or words to look for
 read through the files and make a index (probably reverse) of each unique word linking them to files
 search word then gets a list or lists of files
-- Need to normalise words (work on both caps and not, and remove puncuations) 
-    - Search split and search filename aswell, but use - and _ instead of whitesapce. Different split
+- [x] Integrate file name words in search?
+- [x] Need to normalise words (work on both caps and not, and remove puncuations) 
+    - [x] Search split and search filename aswell, but use - and _ instead of whitesapce. Different split  
 - Ranking the files somehow, future work (word count related to total words and other things)
-- Integrate file name words in search?
 - connecting it to flask, make UI, try out REST
 - AWS option?, learn that and incorporate this project? 
--- main.py
+++ main.py
@@ -4,17 +4,23 @@ import pathlib
 
 
 folder = "test-files"
-search_word = "text".lower()
+
+print("Word to search:")
+search_word_user = input().lower()
+search_word_hard = "text".lower()
 mainDict = {}
 
 
-def add_to_dict(line_array, file):
-    for word in line_array:
+def add_to_dict(word_line, file):
+    word_list = word_line.replace("_", " ").replace("-", " ") \
+    .translate(str.maketrans('', '', string.punctuation)).split() 
+    for word in word_list:
         word = word.lower()
         if word not in mainDict:
             mainDict[word] = {}
         if file.name in mainDict[word]:
-            mainDict[word][file.name] += 1
+            # Counting amount, probebly add aditional variables later for ranking
+            mainDict[word][file.name] += 1 
         else:
             mainDict[word][file.name] = 1
 
@@ -24,18 +30,16 @@ with os.scandir(folder) as files:
         if file.is_file() and file.name.endswith(".txt"):
             with open(file.path, "r") as file:
                 for line in file:   
-                    split_line = line.translate(str.maketrans('', '', string.punctuation)).split()
-                    add_to_dict(split_line, file)
+                    add_to_dict(line, file)
 
                 #Same to file name as for each line
                 file_name = pathlib.Path(file.name).stem
-                file_name = file_name.replace("_", " ").replace("-", " ").split() #File names use - and _ instead of whitespace
                 add_to_dict(file_name, file)
 
 
-print(f"Searched word: {search_word}")
+print(f"Searched word: {search_word_user}")
 print("Files:")
-for file in mainDict.get(search_word, {}):
-    print(f"{file}, appears {mainDict[search_word][file]} times")
+for file in mainDict.get(search_word_user, {}):
+    print(f"{pathlib.Path(file).stem}, {search_word_user} appears {mainDict[search_word_user][file]} times")
 
 
