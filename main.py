06a14d5d68f41fa53bf20c7640293c3667436390
Nuffe <carl.j.paulsson@hotmail.com>
2026-08-10 10:36:04 +0200

    restructuring, moving bm25 into separate functions

--- main.py
+++ main.py
@@ -48,66 +48,53 @@ with os.scandir(folder) as files:
                     
                     add_to_dict(word_list, file_name)
 
-# Look into BM25
-# b = 0.75 k = 1.2
-# Term frequencey (TF) = wordDic[word][file]
-# d = fileDict[file]
-# avgdl = ((for each something) fileDict[file]/file count)
-# IDF, N = number of files and Nt = len(wordDict[word])
+# BM25 with +1
+def BM25():
+    K = 1.2
+    B = 0.75
+    #Term Frequency (TF)
+    BM_Scores = {}
+    for file in wordDict[search_word_user]:   
+
+        length_sum = 0
+        for file2 in fileDict:
+            length_sum += fileDict[file2]
+
+        tf = wordDict[search_word_user][file] # Word frequancy in file
+        d = fileDict[file]                    # document length
+        documentAmount = len(fileDict)        # Total amount of documents
+        nq = len(wordDict[search_word_user])  # Number of documents with word
+        idf = math.log(((documentAmount- nq + 0.5)/(nq + 0.5)) +1) 
+        avgdl = (length_sum / documentAmount) # average length of all documents
+
+        score = (tf * (K + 1)) / (tf + K* (1 - B + B * (d / avgdl)))
+        BM_Scores[file] = score * idf
+    for file in BM_Scores:
+        print(f"{file} BM SCORE: ", BM_Scores[file])
 
 
-k = 1.2
-b = 0.75
-#Term Frequency (TF)
-BM_Scores = {}
-for file in wordDict[search_word_user]:   
-    print("file loop: ", file)
+def call_rank_bm25():
+    BM_list = []
+    with os.scandir(folder) as files:
+        for file in files:
+            if file.is_file() and file.name.endswith(".txt"):
+                with open(file.path, "r") as openFile:
+                    corpus = openFile.read()
+                    corpus = corpus.replace("_", " ").replace("-", " ") \
+                            .translate(str.maketrans('', '', string.punctuation)).lower().split()
+                    BM_list.append(corpus)
 
-    TF = wordDict[search_word_user][file]
-    print("Term frequency:", TF)
-
-    print("Length of document: ", fileDict[file])
-    d = fileDict[file]
-
-    documentAmount = len(fileDict)
-    print("amount of documents in search", documentAmount)
-
-    print("amount of documents with file: ", len(wordDict[search_word_user]))
-    NQ = len(wordDict[search_word_user])
-
-    IDF = math.log(((documentAmount-NQ+0.5)/(NQ+0.5))+1)
-    print("IDF POINT: ", IDF)
-
-    length_sum = 0
-    for file2 in fileDict:
-        length_sum += fileDict[file2]
-
-    avgdl = length_sum / documentAmount
-    print("avgdl: ", avgdl)
-
-    score = (TF * (k + 1)) / (TF + k * (1 - b + b * (d / avgdl)))
-    print(f"BM25 score: {score} * {IDF}: ", score * IDF)
-    BM_Scores[file] = score * IDF
+    print("BM LIST:", BM_list)
+    bm25 = BM25Okapi(BM_list)
+    query = "hello"
+    tokenized_query = query.split(" ")
+    doc_scores = bm25.get_scores(tokenized_query)
+    print("BM25 LIB scores", doc_scores)
 
 
-BM_list = []
-with os.scandir(folder) as files:
-    for file in files:
-        if file.is_file() and file.name.endswith(".txt"):
-            with open(file.path, "r") as openFile:
-                corpus = openFile.read()
-                corpus = corpus.replace("_", " ").replace("-", " ") \
-                        .translate(str.maketrans('', '', string.punctuation)).lower().split()
-                BM_list.append(corpus)
 
-print("BM LIST:", BM_list)
-bm25 = BM25Okapi(BM_list)
 
-query = "hello"
-tokenized_query = query.split(" ")
-doc_scores = bm25.get_scores(tokenized_query)
-print("BM25 LIB scores", doc_scores)
+BM25()
 
-for file in BM_Scores:
-    print(f"{file} BM SCORE: ", BM_Scores[file])
-
+# Bugs to fix:
+# Error when word dont exist
