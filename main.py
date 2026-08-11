5834d1671fc3e2814e5fafc6c3af4c723fa5c6eb
Nuffe <carl.j.paulsson@hotmail.com>
2026-08-11 09:10:54 +0200

    Each file gets a score, even if 0. Fixed problem when word not in any file.

--- main.py
+++ main.py
@@ -14,8 +14,7 @@ fileDict = {}
 
 
 
-def add_to_dict(word_list, file):
-                        
+def add_to_dict(word_list, file):    
     for word in word_list:
         word = word.lower()
         if word not in wordDict:
@@ -27,39 +26,43 @@ def add_to_dict(word_list, file):
             wordDict[word][file] = 1
 
 # make function
-with os.scandir(folder) as files:
-    for file in files:
-        if file.is_file() and file.name.endswith(".txt"):
-            with open(file.path, "r") as openFile:  
+def reverse_index():
+    with os.scandir(folder) as files:
+        for file in files:
+            if file.is_file() and file.name.endswith(".txt"):
+                with open(file.path, "r") as openFile:  
 
-                #Same to file name as for each line
-                file_name = pathlib.Path(file.name).stem
-                file_name_list = file_name.replace("_", " ").replace("-", " ") \
-                    .translate(str.maketrans('', '', string.punctuation)).split()
-                add_to_dict(file_name_list, file_name)
-                
-                for line in openFile:  
-                    word_list = line.replace("_", " ").replace("-", " ") \
+                    #Same to file name as for each line
+                    file_name = pathlib.Path(file.name).stem
+                    file_name_list = file_name.replace("_", " ").replace("-", " ") \
                         .translate(str.maketrans('', '', string.punctuation)).split()
+                    add_to_dict(file_name_list, file_name)
                     
-                    if file_name not in fileDict:
-                        fileDict[file_name] = 0
-                    fileDict[file_name] += len(word_list)
-                    
-                    add_to_dict(word_list, file_name)
+                    for line in openFile:  
+                        word_list = line.replace("_", " ").replace("-", " ") \
+                            .translate(str.maketrans('', '', string.punctuation)).split()
+                        
+                        if file_name not in fileDict:
+                            fileDict[file_name] = 0
+                        fileDict[file_name] += len(word_list)
+                        
+                        add_to_dict(word_list, file_name)
 
 # BM25 with +1
 def BM25():
     K = 1.2
     B = 0.75
-    #Term Frequency (TF)
+
     BM_Scores = {}
+    length_sum = 0
+    for file in fileDict:
+        length_sum += fileDict[file]         # Total length of all files
+        BM_Scores[file] = 0
+
+    if search_word_user not in wordDict:
+        return BM_Scores
+  
     for file in wordDict[search_word_user]:   
-
-        length_sum = 0
-        for file2 in fileDict:
-            length_sum += fileDict[file2]
-
         tf = wordDict[search_word_user][file] # Word frequancy in file
         d = fileDict[file]                    # document length
         documentAmount = len(fileDict)        # Total amount of documents
@@ -69,8 +72,7 @@ def BM25():
 
         score = (tf * (K + 1)) / (tf + K* (1 - B + B * (d / avgdl)))
         BM_Scores[file] = score * idf
-    for file in BM_Scores:
-        print(f"{file} BM SCORE: ", BM_Scores[file])
+    return BM_Scores
 
 
 def call_rank_bm25():
@@ -93,8 +95,10 @@ def call_rank_bm25():
 
 
 
+reverse_index()
+score = BM25()
+call_rank_bm25()
 
-BM25()
-
-# Bugs to fix:
-# Error when word dont exist
+print("SCORE:", score)
+asc = {k: v for k, v in sorted(score.items(), key=lambda item: item[1], reverse=True)}
+print(asc)
