0de8cb5918563c55f416e02a15ddf71694a8fba6
Nuffe <carl.j.paulsson@hotmail.com>
2026-08-13 09:38:41 +0200

    start at api to connect to flask site for UI

---
+++ api.py
@@ -0,0 +1,22 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+origins = [
+    "http://localhost",
+    "http://localhost:8080",
+    "http://localhost:7000"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get("/")
+def connect_test():
+    return {"message": " Hello from document search"}
